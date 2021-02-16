d24a7b0a16e3de8df459f669398e6ee21e3acb59
17021084 <dotrunghg1999@gmail.com>
2021-02-16 10:06:18 +0700

    setup

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import * as firebase from "firebase";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -8,6 +9,22 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Landing from "./components/auth/Landing";
 
 const Stack = createStackNavigator();
+
+// hide this in env file in furture
+const firebaseConfig = {
+  apiKey: "AIzaSyBG0PpStd7JHoSzY14Fs-crU5ZKI1aryH0",
+  authDomain: "instagram-5ca5e.firebaseapp.com",
+  projectId: "instagram-5ca5e",
+  storageBucket: "instagram-5ca5e.appspot.com",
+  messagingSenderId: "446228072719",
+  appId: "1:446228072719:web:f71959472226550d1d167a",
+  measurementId: "G-BMJSLEMG0Z",
+};
+
+// make sure that no instance run when initializel App
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
