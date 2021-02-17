8774997dbc2d89a292be48efccf5c9a305d3bdd7
17021084 <dotrunghg1999@gmail.com>
2021-02-17 16:58:13 +0700

    fix some input method

--- components/main/Save.js
+++ components/main/Save.js
@@ -1,11 +1,56 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  Button,
+  Dimensions,
+} from "react-native";
 
-export default function Save(props) {
-  console.log(props);
+const windowWidth = Dimensions.get("window").width;
+const containerPadding = 10;
+
+export default function Save({ route }) {
+  const image = route.params.image;
+  const [caption, setCaption] = useState("");
+  //fix keyboard hide in put field, search :KeyboardAvoidingView
   return (
-    <View>
-      <Text>Save screen</Text>
+    <View style={styles.container}>
+      <TextInput
+        onChangeText={(text) => {
+          setCaption(text);
+        }}
+        value={caption}
+        placeholder="What are you thinking ?"
+        style={styles.input}
+        multiline={true}
+      />
+      <Image source={{ uri: image }} style={styles.image} />
+      <Button style={styles.button} title="upload" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: containerPadding,
+  },
+  image: {
+    width: windowWidth - 2 * containerPadding,
+    height: windowWidth,
+    borderRadius: 30,
+    marginVertical: 20,
+  },
+  input: {
+    paddingHorizontal: 20,
+    marginVertical: 20,
+    height: 150,
+    borderColor: "pink",
+    borderWidth: 1,
+    borderRadius: 30,
+  },
+});
