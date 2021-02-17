d24ad5a64c6e5fed5f3c877ee68e7265f4782bd1
17021084 <dotrunghg1999@gmail.com>
2021-02-17 13:54:28 +0700

    set up camera and take picture :camera:

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 import { Camera } from "expo-camera";
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -12,6 +14,14 @@ export default function App() {
       setHasPermission(status === "granted");
     })();
   }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await camera.takePictureAsync(options);
+      setImage(data.uri);
+    }
+  };
 
   if (hasPermission === null) {
     return <View />;
@@ -21,45 +31,60 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}
-          >
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+      <Camera
+        ref={(ref) => {
+          setCamera(ref);
+        }}
+        style={styles.camera}
+        type={type}
+        ratio={"16:9"}
+      ></Camera>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="FLip button"
+          style={styles.button}
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        />
+        <Button
+          title="shoot"
+          style={styles.button}
+          onPress={() => {
+            takePicture();
+          }}
+        />
+        {image && (
+          <Image source={{ uri: image }} style={styles.previewPicture} />
+        )}
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "black",
   },
   camera: {
     flex: 1,
   },
   buttonContainer: {
-    flex: 1,
-    backgroundColor: "transparent",
+    justifyContent: "space-around",
+    alignItems: "center",
     flexDirection: "row",
     margin: 20,
   },
   button: {
-    flex: 0.1,
     alignSelf: "flex-end",
     alignItems: "center",
   },
-  text: {
-    fontSize: 18,
-    color: "white",
+  previewPicture: {
+    height: 100,
+    width: 100,
   },
 });
