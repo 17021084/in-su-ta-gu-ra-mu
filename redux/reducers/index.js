80ad9c7e85419d0e6720c3f8b17f498931aaccbd
17021084 <dotrunghg1999@gmail.com>
2021-02-21 10:10:22 +0700

    set up redux for save following usersdata

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import UserReducer from "./UserReducer";
+import UsersReducer from "./UsersReducer";
 
- 
 export default combineReducers({
   userState: UserReducer,
+  usersState: UsersReducer,
 });
